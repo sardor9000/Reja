dd26fcc47e1de22c63bd9c32294725a5cb54ca4f
Sardor <usok201802@gmail.com>
2023-09-22 23:16:34 +0900

    Building Server

---
+++ server.js
@@ -0,0 +1,32 @@
+console.log("web server ishlayapti");
+const express = require("express");
+const app = express();
+const http = require("http");
+
+// 1. Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+
+// 2. Session code
+// 3. Views code 
+app.set("view", "views");
+app.set("view engine", "ejs");
+
+
+// 4. Routing code 
+app.get("/hello", function (req, res){
+    res.end(`<h1>HELLO WORLD</h1>`);
+});
+
+app.get("/gift", function(req, res){
+    res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
+})
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function(){
+    console.log(`The server is successfully on port: ${PORT}`);
+})
