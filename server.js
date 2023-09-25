1188c1c5b7108d789234b9137888b85a5f3da393
Sardor <usok201802@gmail.com>
2023-09-25 14:03:37 +0900

    feat: author router ejs page

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("web server ishlayapti");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err){
+        console.log("ERROR:", err);
+    }else{
+        user = JSON.parse(data)
+    }
+});
 
 // 1. Kirish code
 app.use(express.static("public"));
@@ -23,16 +33,20 @@ app.post("/create-item", (req, res) => {
 })
 
 
+app.get("/author", (req, res)=>{
+    res.render("author", {user: user} )
+});
+
+
 app.get("/", function (req, res){
     res.render('harid')
 });
-
 
 const server = http.createServer(app);
 let PORT = 4000;
 server.listen(PORT, function() {
     console.log(`The server is successfully on port: ${PORT}`);
-})
+});
 
 
 
