c1809b6689c4ab075ee9d2e0ada06b086ce2e032
Sardor <usok201802@gmail.com>
2023-10-02 12:46:00 +0900

    feat: FrontEnd JavaScriptni qurish

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ console.log("web server ishlayapti");
 const express = require("express");
 const app = express();
 
-const fs = require("fs");
+// const fs = require("fs");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err){
-        console.log("ERROR:", err);
-    }else{
-        user = JSON.parse(data)
-    }
-}); 
+// let user;
+// fs.readFile("database/user.json", "utf8", (err, data) => {
+//     if(err){
+//         console.log("ERROR:", err);
+//     }else{
+//         user = JSON.parse(data)
+//     }
+// }); 
 
 // MongoDB calling
 
@@ -38,12 +38,7 @@ app.post("/create-item", (req, res) => {
     console.log(req.body);
     const new_reja = req.body.reja;
     db.collection('plans').insertOne({reja: new_reja}, (err, data) =>{
-        if(err){
-            console.log(err);
-            res.end('something went wrong'); 
-        }else{
-            res.end('successfully added') 
-        }
+       res.json(data.ops[0])
     })
    // code
 })
